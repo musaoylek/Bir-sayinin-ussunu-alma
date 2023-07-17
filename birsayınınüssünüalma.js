b503c93548f1e3e874d94576b8d4398d86292c2f
musaoylek <101888206+musaoylek@users.noreply.github.com>
2023-07-17 19:19:24 +0300

    birsayınınüssünüalma.js

---
+++ birsayınınüssünüalma.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>musaoylek math.pow() metodu kullanımı</title>
+</head>
+<body>	
+<script>			
+var sayi1, sayi2;		
+sayi1= prompt("Lütfen taban sayısını giriniz");
+sayi2= prompt("Lütfen üs sayısını giriniz");
+var us= Math.pow(sayi1,sayi2);	
+
+alert("İşlemin sonucu: " +us);	</script>
+</body>
+</html>
